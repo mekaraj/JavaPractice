7c141fe60f9c61346616f784c9ff08f5219bd707
U-Prashant-PC\Prashant <Prashant@Prashant-PC.(none)>
2013-05-15 21:13:44 +0100

    First commit

---
+++ src/MinElementSortedArray.java
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Prashant
+ * Date: 5/15/13
+ * Time: 9:11 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MinElementSortedArray {
+
+}
